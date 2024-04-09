6b8d4ac532d0acf19a88d7e461069189a0587b75
franck <carnet.franck.paul@gmail.com>
2024-04-09 10:46:30 +0200

    New release 6.2

--- _define.php
+++ _define.php
@@ -12,7 +12,7 @@ $this->registerModule(
     'zh2_auto',
     'Zen habits (based on eponym Wordpress theme designed by Leo Babuta)',
     'Franck Paul',
-    '6.1',
+    '6.2',
     [
         'requires'           => [['core', '2.30']],
         'type'               => 'theme',
