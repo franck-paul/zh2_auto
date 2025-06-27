19bbfac9dd97535170f867d534152eef06026499
franck <carnet.franck.paul@gmail.com>
2025-06-27 13:36:24 +0200

    New release 7.0

--- _define.php
+++ _define.php
@@ -13,15 +13,17 @@ $this->registerModule(
     'zh2_auto',
     'Zen habits (based on eponym Wordpress theme designed by Leo Babuta)',
     'Franck Paul',
-    '6.3',
+    '7.0',
     [
-        'date'               => '2003-08-13T13:42:00+0100',
-        'requires'           => [['core', '2.30']],
+        'date'               => '2025-06-27T13:36:24+0200',
+        'requires'           => [['core', '2.35']],
         'type'               => 'theme',
         'information_config' => true,
+        'overload'           => true,
 
         'details'    => 'https://open-time.net/?q=zh2_auto',
         'support'    => 'https://github.com/franck-paul/zh2_auto',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/zh2_auto/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
--- src/FrontendBehaviors.php
+++ src/FrontendBehaviors.php
@@ -19,7 +19,7 @@ use ArrayObject;
 
 class FrontendBehaviors
 {
-    public static function tplIfConditions(string $tag, array $attr, string $content, ArrayObject $if): string
+    public static function tplIfConditions(string $tag, array|ArrayObject $attr, string $content, ArrayObject $if): string
     {
         if ($tag === 'SysIf' && isset($attr['has_blog_descr'])) {
             $sign = (bool) $attr['has_blog_descr'] ? '' : '!';
